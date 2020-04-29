93562dcc34a095da94fc75f53372d7a8984dc5ed
MatijaMi <64535541+MatijaMi@users.noreply.github.com>
2020-04-29 17:01:20 +0200

    Improved functions
    Added function to find value for a tag in a given IFD, needs further work for all types

--- script.js
+++ script.js
@@ -22,43 +22,49 @@ function fileAdded() {
 	
 	  reader.readAsArrayBuffer(file);
   };
- 
-function findIFDZeroLegth(){
 
-	var firstbyte = bytes[16];
-	var secondbyte = bytes[17]*256;
-	//console.log(firstbyte);
-	//console.log(secondbyte);
-	return firstbyte+secondbyte;
-}
 
 function collectData(){
-	
+	//Variables for working with IFDZero
 	var ifd0Entries = [];
-	var ifdLength = findIFDZeroLegth();
+	var ifdZeroOffset = 16;
+	var modelName = findIFDTagValue(ifdZeroOffset, 16,1,true,true);
 	
-	var model = findIFDEntries(ifdLength,18, 16, 1);
-	var modelOffset =transformFourBytes.apply(null,model.slice(8,12));
-	var modelLength = transformFourBytes.apply(null, model.slice(4,8))
-	var modelName =findModelName(modelOffset, modelLength);
+	//Code for finding the EXIF Sub-IFD
+	var exifOffset = findIFDTagValue(ifdZeroOffset,105,135,false,false);
+	var makerNoteOffset = findIFDTagValue(exifOffset,124,146,false,false);
+	
+	
+	
 	ifd0Entries.push(modelName);
-	
-	var exif = findIFDEntries(ifdLength, 105, 135);
-	
-	
-	
-	
+	ifd0Entries.push(" "+ exifOffset+" ");
+	ifd0Entries.push(makerNoteOffset);
 	document.getElementById("ifd0").innerHTML= '<ul>' + ifd0Entries.join('') + '</ul>';	
+	
+} 
+
+
+
+function findIFDTagValue(ifdOffset,tagID1, tagID2, isOffsetToValue, isValueString){
+	var ifdLength = transformTwoBytes(bytes[ifdOffset],bytes[ifdOffset+1]);
+	var tag = findIFDEntries(ifdOffset, ifdLength,tagID1,tagID2);
+	
+	if(isOffsetToValue){
+		var offsetValue = transformFourBytes.apply(null,tag.slice(8,12));
+		var valueLength = transformFourBytes.apply(null,tag.slice(4,8))
+		if(isValueString){
+			return findNameValue(offsetValue,valueLength);
+		}
+	}else{
+		return  transformFourBytes.apply(null,tag.slice(8,12));
+	}
+	
+	
+	
 }
 
 
-function transformFourBytes(byte1, byte2,byte3,byte4){
-	
-	return byte1+ byte2*Math.pow(2,8) + byte3*Math.pow(2,16) + byte4*Math.pow(2,24);
-	
-}
-
-function findModelName(modelOffset, modelLength){
+function findNameValue(modelOffset, modelLength){
 	var model = "";
 	for(var i =0; i< modelLength; i++){
 		model= model +String.fromCharCode(bytes[modelOffset+i]);
@@ -69,18 +75,32 @@ function findModelName(modelOffset, modelLength){
 
 
 
-function findIFDEntries(ifdLength, ifdOffset, ID1, ID2){
+function findIFDEntries(ifdOffset, ifdLength, ID1, ID2){
 	
 	var ifdEntry = [];
+	
 	for(var i =0; i <ifdLength; i++){
-		var tagID1 = bytes[ifdOffset+12*i];
-		var tagID2 = bytes[ifdOffset+1+12*i]; 
+		var tagID1 = bytes[ifdOffset+2+12*i];
+		var tagID2 = bytes[ifdOffset+3+12*i]; 
 			
 		if(tagID1==ID1 && tagID2==ID2){
 			for(var j =0; j <12; j++){
-				ifdEntry.push(bytes[18+j+12*i]);
+				ifdEntry.push(bytes[ifdOffset+2+j+12*i]);
 			}
 		}	
 	}
+	
 	return ifdEntry;
+}
+
+ 
+function transformTwoBytes(byte1, byte2){
+	return byte1 + byte2*256;
+}
+
+
+function transformFourBytes(byte1,byte2,byte3,byte4){
+	
+	return byte1+ byte2*Math.pow(2,8) + byte3*Math.pow(2,16) + byte4*Math.pow(2,24);
+	
 }
