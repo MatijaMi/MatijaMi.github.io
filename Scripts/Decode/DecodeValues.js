6486238f936f822221fb27c6a9ec4349d63bc68c
MatijaMi <64535541+MatijaMi@users.noreply.github.com>
2020-05-25 09:59:25 +0200

    Found mistake that caused artifacting, needs to implemented everywhere

--- Scripts/Decode/DecodeValues.js
+++ Scripts/Decode/DecodeValues.js
@@ -1,48 +1,76 @@
-function decompress2V(numberOfLines,HSF,sliceDimensions,samplePrecision,ht1,ht2){
+function decompress2V(numberOfLines,HSF,sliceDimensions,samplePrecision,ht1,ht2,samplesPerLine){
 	var imageLines=[];
 	for(var k =0; k <numberOfLines;k++){
 		imageLines.push([]);
 	}
-	for(var j =0; j< sliceDimensions[0]+1;j++){
-		console.log("Slice " +j);
-		var pbPart=Math.floor(100/(sliceDimensions[0]+1));
-		var numberOfSamples;
-		
-		
-		if(j==sliceDimensions[0]){
-			numberOfSamples= sliceDimensions[2]/HSF;
-		}else{
-			numberOfSamples= sliceDimensions[1]/HSF;
+	
+	var width = samplesPerLine;
+	console.log(width);
+	var height= numberOfLines;
+	var acWidth= width/HSF;
+	var prevC1=Math.pow(2,samplePrecision-1);
+	var prevC2=Math.pow(2,samplePrecision-1);
+	
+	for(var i =0; i < height;i++){
+		if(i>0){
+			prevC1=imageLines[i-1][0];
+			prevC2=imageLines[i-1][1];
 		}
-				
-		var counter = numberOfSamples*numberOfLines;
-		console.log("Counter =" +counter);
-		var i =0;
-		
-		var prevC1=Math.pow(2,samplePrecision-1);
-		var prevC2=Math.pow(2,samplePrecision-1);
-		while(i<counter){
-			
-			if(i%(Math.floor(counter/pbPart))==0){
-				postMessage(["PB", (j*pbPart+(i/(Math.floor(counter/pbPart)))),"Extracting Values"]);
-			}
-			
-			if(i>0 && i%numberOfSamples==0){
-				prevC1=imageLines[Math.floor(i/numberOfSamples)-1][0];
-				prevC2=imageLines[Math.floor(i/numberOfSamples)-1][1];
-			}
+		for(var j =0; j <width;j++){
 			prevC1 = findNextValue(ht1, prevC1);
 			prevC1= limitComponent(prevC1,samplePrecision);
 			
 			prevC2 = findNextValue(ht2, prevC2);
 			prevC2= limitComponent(prevC2,samplePrecision);
 			
-			imageLines[Math.floor(i/numberOfSamples)].push(prevC1);
-			imageLines[Math.floor(i/numberOfSamples)].push(prevC2);
-			i+=2;
-		}	
+			imageLines[i].push(prevC1);
+			imageLines[i].push(prevC2);	
+		}
 	}
-	return imageLines
+	console.log(imageLines.length);
+	console.log(imageLines[imageLines.length-1].length);
+	var image= unslice(imageLines, sliceDimensions,numberOfLines,samplesPerLine);
+	return image;
+}
+
+function cropBorders(image, top, left, bot , right){
+	var croppedImage =[];
+	for(var i=top;i<bot;i++){
+		
+		croppedImage.push(image[i].slice(left,right));
+	}
+	console.log("DONE");
+	return croppedImage;
+}
+
+function unslice(image, slices, height,widthx){
+	var imageLines=[];
+	for(var k =0; k <height;k++){
+		imageLines.push([]);
+	}
+	var width=widthx*2;
+	var slice1=height*slices[1];
+	
+	for(var j=0; j<slices[0]+1;j++){
+		if(j==slices[0]){
+			numberOfSamples= slices[2];
+		}else{
+			numberOfSamples= slices[1];
+		}
+				
+		var counter = numberOfSamples*height;
+		console.log("COUNTER " +counter);
+		for(var i =0; i<counter;i++){
+			var z=j*slice1+i;
+			var x = image[Math.floor(z/width)][z%width];
+			imageLines[Math.floor(i/numberOfSamples)].push(x);
+		}
+		console.log("LENGTH " +imageLines[1].length);
+		console.log(j);
+	}
+	
+	return imageLines;
+	
 }
 function decompress3V(numberOfLines,HSF,sliceDimensions,samplePrecision,ht1,ht2){
 	var imageLines=[];
--- Scripts/Decode/RGGB.js
+++ Scripts/Decode/RGGB.js
@@ -5,15 +5,16 @@ function decompressRGGB(data, mData){
 	var ht1 =metaData.get("HT1");
 	var ht2 =metaData.get("HT2");
 	var numberOfLines = metaData.get("SOF3").get("NumberOfLines");
-	var samplesPerLine = metaData.get("SOF3").get("samplesPerLine");
+	var samplesPerLine = metaData.get("SOF3").get("SamplesPerLine");
 	var HSF = metaData.get("SOF3").get("HSF");
 	var sliceDimensions = metaData.get("Slices");
 	var samplePrecision = metaData.get("SOF3").get("SamplePrecision");
 	var numberOfComponents = metaData.get("SOF3").get("ImageComponents");
 	
+	
 	window.bitPointer=0;
 	if(numberOfComponents==2){
-		 var imageLines=decompress2V(numberOfLines,HSF,sliceDimensions,samplePrecision,ht1,ht2);
+		 var imageLines=decompress2V(numberOfLines,HSF,sliceDimensions,samplePrecision,ht1,ht2,samplesPerLine);
 	}else{
 		 var imageLines=decompress4V(numberOfLines,HSF,sliceDimensions,samplePrecision,ht1,ht2);
 	}
@@ -24,14 +25,15 @@ function decompressRGGB(data, mData){
 }
 
 function limitComponent(comp, sp){
+	//var maxValue =16100;
 	var maxValue =Math.pow(2,sp)-1;
 	
 	if(comp>maxValue){
-		//comp=2*maxValue-comp;
-		comp=maxValue;
+		comp=comp%(maxValue+1);
+		//comp=maxValue;
 	}else{
 		if(comp<0){
-			comp=0;
+			comp=comp+maxValue;
 		}
 	}
 	return comp;
