f173c9387125f41aa8a802112ae21747e7fc22de
MatijaMi <64535541+MatijaMi@users.noreply.github.com>
2020-06-01 19:04:30 +0200

    More refactoring and comments, prettier code :D

--- Scripts/Utils/DecodeUtils.js
+++ Scripts/Utils/DecodeUtils.js
@@ -1,68 +1,80 @@
+/*	Given a position in the bit array finds the next value
+	by checking if the sequence of the next bits is in the Huffman Table, 
+	if it isn't it adds the next bit and checks again */
 function findNextValue(huffTable, previousValue){
 	
 	var byte = Math.floor(bitPointer/8);
 	var bit = bitPointer%8;
 	var i =0;
 	
+	/* Seeing as  the code can be in multiple bytes, we have 
+		to beware where to search, worst case we search in 3 bytes */
+	var currentCode;
 	while(i<16){
 		i++;
 		if(bit+i<=8){
-			if(typeof huffTable.get(bits[byte].substring(bit,bit+i)) !== "undefined"){
-				var dcL = bits[byte].substring(bit,bit+i);
+			currentCode=bits[byte].substring(bit,bit+i);
+			if(typeof huffTable.get(currentCode) !== "undefined"){
 				break;
 			}		
 		}else{
 			if(bit+i<=16){
-				if(typeof huffTable.get(bits[byte].substring(bit)+bits[byte+1].substring(0,bit+i-8)) !== "undefined"){
-					var dcL =bits[byte].substring(bit)+bits[byte+1].substring(0,bit+i-8);
+				currentCode=bits[byte].substring(bit)+bits[byte+1].substring(0,bit+i-8);
+				if(typeof huffTable.get(currentCode) !== "undefined"){
 					break;
 				}
-		}else{
-			if(typeof huffTable.get(bits[byte].substring(bit)+bits[byte+1]+ bits[byte+2].substring(0,bit+i-16)) !== "undefined"){
-				var dcL = bits[byte].substring(bit)+bits[byte+1]+ bits[byte+2].substring(0,bit+i-16);
+			}else{
+				currentCode=bits[byte].substring(bit)+bits[byte+1]+ bits[byte+2].substring(0,bit+i-16);
+				if(typeof huffTable.get(currentCode) !== "undefined"){
 					break;
-			}	
-		}
+				}	
+			}
 		}
 	}
-	
-	
+
 	bitPointer=bitPointer+i;
-	var x =huffTable.get(dcL);
-	
-	return previousValue+getDifferenceCode(getNextBits(x));
+	var differenceCodeLength =huffTable.get(currentCode);
+	//The next value is equal to the previous value + the difference value
+	return previousValue+getDifferenceValue(getNextBits(differenceCodeLength));
 }
 
-
-function getDifferenceCode(differenceBits){
+/*	Given a sequence of bits, calculates their value based
+	on the system used for difference values:
+	-The first bit represents the sign, 1 is minus and 0 plus
+	-If negative the value is equal to -1 + 2^lengthOfDifferenceCode+ bitsWithoutSign.toDecimal
+	-If positive the value is equal to 2^(lengthOfDifferenceCode-1)+ bitsWithoutSign.toDecimal */
+function getDifferenceValue(differenceBits){
 	var number;
-	if(differenceBits.length==0){
+	if(differenceBits.length==0){//If zero then there is no difference
 		return 0;
 	}
-	if (differenceBits.length==1){
+	if (differenceBits.length==1){//For length one it's either -1 or 1
 		bitPointer=bitPointer+1;
 		return parseInt(differenceBits)*2-1;
 	}
 	
-	var addition = parseInt(differenceBits.substring(1),2);
+	var restBits = parseInt(differenceBits.substring(1),2);
 	
-	if (differenceBits.charAt(0)==0){
-			number = (1 - Math.pow(2,differenceBits.length))+addition;
+	if (differenceBits.charAt(0)==0){//Sign
+			number = (1 - Math.pow(2,differenceBits.length))+restBits;
 		}else{
-			number = Math.pow(2,differenceBits.length-1)+addition;			
+			number = Math.pow(2,differenceBits.length-1)+restBits;			
 		}
 	bitPointer=bitPointer+differenceBits.length;
 	return number;
 }
 
-function getNextBits(length){
+//Returns the next n bits
+function getNextBits(n){
 	var str="";
-	for(var i =0; i<length;i++){
+	for(var i =0; i<n;i++){
 		str=str+bits[Math.floor((bitPointer+i)/8)].charAt((bitPointer+i)%8);	
 	}
 	return str;
 }
 
+/*	Depending on the number of components, returns an array with
+	the proper Huffman Table for each component */
 function setupHTS(sos,ht1,ht2,numberOfComponents){
 	var hts=[];
 	for(var k =0; k <numberOfComponents;k++){
@@ -74,10 +86,10 @@ function setupHTS(sos,ht1,ht2,numberOfComponents){
 	}
 	return hts;
 }
-
+//Returns how the components and  how many of their parts are saved in the bits
 function setupComponentParts(HSF,VSF){
 	if(HSF==1){
-		var compParts=[1,1,1,1];
+		var compParts=[1,1];
 	}else{
 		if(VSF==1){
 			var compParts=[2,1,1];
@@ -88,6 +100,9 @@ function setupComponentParts(HSF,VSF){
 	return compParts;
 }
 
+/*	Initialises the previous values
+	R,G,B and Y : 2^(samplePrecision-1)
+	Cb and Cr : 0 */
 function setPreviousValues(nComponents,samplePrecision){
 	var previousValues = [];
 	for(var k =0; k<nComponents;k++){
@@ -100,6 +115,8 @@ function setPreviousValues(nComponents,samplePrecision){
 	return previousValues;
 }
 
+/*	Returns the number of entries in one sequence of 
+	values before repeating(RGGB=2;YCC=4;YYYYCC=6) */
 function getNumberOfEntries(nComponents,HSF,VSF){
 	if(HSF==1){
 		return nComponents;
@@ -112,7 +129,9 @@ function getNumberOfEntries(nComponents,HSF,VSF){
 	}
 }
 
-
+/* 	Due to the borders of the camera's sensors not always
+	being exactly on the edge of the image, removes black stripes
+	 on the edges where there were no sensors from the image */
 function cropBorders(image, top, left, bot, right){
 	var croppedImage =[];
 	for(var i=top;i<bot;i++){
@@ -121,7 +140,9 @@ function cropBorders(image, top, left, bot, right){
 	return croppedImage;
 }
 
-
+/*	Based on the White Balance values from the MakerNote,
+	calculates the ratios between the colors by using the biggest element
+	to decrease file size */
 function applyWhiteBalance(image,metaData){
 	var wb = metaData.get("WhiteBalance");
 	var max=wb[0];
