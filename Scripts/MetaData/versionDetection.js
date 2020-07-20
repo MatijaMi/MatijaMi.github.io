7ec77c8bab79f659bef367ae8641a5cf45610025
MatijaMi <64535541+MatijaMi@users.noreply.github.com>
2020-07-20 10:03:13 +0200

    Testing XMLHttp

--- Scripts/MetaData/versionDetection.js
+++ Scripts/MetaData/versionDetection.js
@@ -1,15 +1,13 @@
 function detectColorDataVersion(cameraModel){
 	console.log(cameraModel);
-	/*)
-	let cd1=["20D","350D"];
-	let cd2=["1D Mark II", "1Ds Mark II"];
-	let cd3=["1D Mark II N", "5D", "30D","100D"];
-	let cd4=["1D Mark III", "1Ds Mark III", "1D Makr IV", "5D Mark II","7D","40D","50D", "60D", "450D", "500D", "550D", "1000D","1100D"];
-	let cd5=["EOS M", "PowerShot"];
-	let cd6=["600D", "1200D"];
-	let cd7=["1DX","5D Mark III", "6D","100D","650D","700D","7D Mark II"];
-	let cd8=["5DS", "5DSR","80D","1300D","2000D","4000D", "6D Mark II", "77D", "200D", "800D"];
-	let cd9=["M50","EOS R", "EOS RP", "90D", "M6 Mark II", "M200"];
-	*/
+	let requestURL= "https://github.com/MatijaMi/MatijaMi.github.io/Data/colorDataVersion.js"
+	let request = new XMLHttpRequest();
+	request.open('GET', requestURL);
+	request.responseType = 'json';
+	request.send();
+	request.onload = function() {
+		let cdv = request.response;
+		console.log(cdv);
+	}
 	
 }
